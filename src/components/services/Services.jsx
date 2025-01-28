e5852df110b179248b25bd3f90873aa2e1aa5e49
Mohammed Suyel <ciphers.developers@gmail.com>
2025-01-28 16:44:29 +0400

    service done

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,6 +1,14 @@
+import React, { useState } from 'react';
 import './services.css';
 
 const Services = () => {
+
+    const [viewService, setViewService] = useState(0);
+
+    const setViewState = (index) => {
+        setViewService(index);
+    }
+
     return (
         <section className="services section" id="services">
             <h2 className="section__title">Services</h2>
@@ -14,23 +22,171 @@ const Services = () => {
                     </div>
 
 
-                    <span className="services__button">View More <i className="uil uil-arrow-right services__button-icon"></i></span>
+                    <span className="services__button" onClick={ ()=> setViewState(1) } >View More <i className="uil uil-arrow-right services__button-icon"></i></span>
 
 
-                    <div className="services__modal">
+                    <div className={viewService === 1 ? "services__modal active-modal" : "services__modal"} >
                         <div className="services__modal-content">
-                        <i className="uil uil-times services__modal-close"></i>
-                            <h2 className="services__modal-title">Web Development</h2>
+                        <i onClick={ () => setViewState(0) } className="uil uil-times services__modal-close"></i>
+                            <h3 className="services__modal-title">Web Development</h3>
                             
                             <p className="services__modal-description">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+                                Services with more than 3 years of experience. Providing quality work to clients and companies.
                             </p>
 
                             <ul className="services__modal-services grid">
                                 <li className="services__modal-service">
                                     <i className="uil uil-check-circle services__modal-icon"></i>
                                     <p className="services__modal-info">
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+                                        I develop user interface.
+                                    </p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">
+                                        Web page development
+                                    </p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">
+                                        I create ux element interaction
+                                    </p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">
+                                        I position your company brand
+                                    </p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">
+                                        Design and mockups of products for companies
+                                    </p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="services__content">
+                    <div>
+                        <i className="uil uil-web-grid services__icon"></i>
+                        <h3 className="services__title">Web Development</h3>
+                    </div>
+
+
+                    <span className="services__button" onClick={ ()=> setViewState(2) } >View More <i className="uil uil-arrow-right services__button-icon"></i></span>
+
+
+                    <div className={viewService === 2 ? "services__modal active-modal" : "services__modal"} >
+                        <div className="services__modal-content">
+                        <i onClick={() => setViewService(0)} className="uil uil-times services__modal-close"></i>
+                            <h3 className="services__modal-title">Web Development</h3>
+                            
+                            <p className="services__modal-description">
+                                Services with more than 3 years of experience. Providing quality work to clients and companies.
+                            </p>
+
+                            <ul className="services__modal-services grid">
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">
+                                        I develop user interface.
+                                    </p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">
+                                        Web page development
+                                    </p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">
+                                        I create ux element interaction
+                                    </p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">
+                                        I position your company brand
+                                    </p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">
+                                        Design and mockups of products for companies
+                                    </p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="services__content">
+                    <div>
+                        <i className="uil uil-web-grid services__icon"></i>
+                        <h3 className="services__title">Web Development</h3>
+                    </div>
+
+
+                    <span className="services__button" onClick={ ()=> setViewState(3) }>View More  <i className="uil uil-arrow-right services__button-icon"></i></span>
+
+
+                    <div className={viewService === 3 ? "services__modal active-modal" : "services__modal"}>
+                        <div className="services__modal-content">
+                        <i onClick={() => setViewService(0)} className="uil uil-times services__modal-close"></i>
+                            <h3 className="services__modal-title">Web Development</h3>
+                            
+                            <p className="services__modal-description">
+                                Services with more than 3 years of experience. Providing quality work to clients and companies.
+                            </p>
+
+                            <ul className="services__modal-services grid">
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">
+                                        I develop user interface.
+                                    </p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">
+                                        Web page development
+                                    </p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">
+                                        I create ux element interaction
+                                    </p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">
+                                        I position your company brand
+                                    </p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">
+                                        Design and mockups of products for companies
                                     </p>
                                 </li>
                             </ul>
--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -9,25 +9,25 @@ const Frontend = () => {
         <div className="skills__box">
             <div className="skills__group">
                 <div className="skills__data">
-                    <i class='bx bx-badge-check' ></i>
+                    <i className='bx bx-badge-check' ></i>
                     <h3 className="skills__name">MERN</h3>
                     <span className="skills__level">Intermediate</span>
                 </div>
 
                 <div className="skills__data">
-                    <i class='bx bx-badge-check'></i>
+                    <i className='bx bx-badge-check'></i>
                     <h3 className="skills__name">HTML</h3>
                     <span className="skills__level">Basic</span>
                 </div>
 
                 <div className="skills__data">
-                    <i class='bx bx-badge-check' ></i>
+                    <i className='bx bx-badge-check' ></i>
                     <h3 className="skills__name">CSS</h3>
                     <span className="skills__level">Advance</span>
                 </div>
 
                 <div className="skills__data">
-                    <i class='bx bx-badge-check' ></i>
+                    <i className='bx bx-badge-check' ></i>
                     <h3 className="skills__name">Javascript</h3>
                     <span className="skills__level">Intermediate</span>
                 </div>
@@ -35,25 +35,25 @@ const Frontend = () => {
 
             <div className="skills__group">
                 <div className="skills__data">
-                    <i class='bx bx-badge-check' ></i>
+                    <i className='bx bx-badge-check' ></i>
                     <h3 className="skills__name">Git</h3>
                     <span className="skills__level">Advance</span>
                 </div>
 
                 <div className="skills__data">
-                    <i class='bx bx-badge-check'></i>
+                    <i className='bx bx-badge-check'></i>
                     <h3 className="skills__name">React</h3>
                     <span className="skills__level">Intermediate</span>
                 </div>
 
                 <div className="skills__data">
-                    <i class='bx bx-badge-check' ></i>
+                    <i className='bx bx-badge-check' ></i>
                     <h3 className="skills__name">PHP</h3>
                     <span className="skills__level">Intermediate</span>
                 </div>
 
                 <div className="skills__data">
-                    <i class='bx bx-badge-check' ></i>
+                    <i className='bx bx-badge-check' ></i>
                     <h3 className="skills__name">Bootstrap</h3>
                     <span className="skills__level">Intermediate</span>
                 </div>
