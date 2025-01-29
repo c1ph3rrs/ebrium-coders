da6831d47c77a4d762a753a6bf9ed15021c6fc6d
Ibrahim Akram Mughal <68148781+c1ph3rrs@users.noreply.github.com>
2025-01-30 00:06:20 +0400

    some changes

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './contact.css';
+
+const Contact = () => {
+    return (
+        <section className="contact section" id="contact">
+            <h2 className="section__title">Contact Me</h2>
+        </section>
+    );
+};
+
+export default Contact;
